0a3c0362ee6cd9f81375c48245c2a053a49b33d5
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2017-03-18 16:30:19 +0900

    [add] Life cycle check. Add method is onStart(), onResume(), onPause(), onRestart(), onStop() and onDestroy().

--- app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
@@ -2,6 +2,7 @@ package jp.ac.chiba_fjb.asaka.ref.sdkrefs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v("LIFECYCLE", "onRestart()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("LIFECYCLE", "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("LIFECYCLE", "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v("LIFECYCLE", "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("LIFECYCLE", "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("LIFECYCLE", "onDestory()");
     }
 }
