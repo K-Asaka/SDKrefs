a5c9be7e4be85ec13c690ae9d760c0b0c46f9b17
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2017-04-01 09:26:57 +0900

    [add][mod] Added menu.xml. Code changed onCreateOptionsMenu().

--- app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
@@ -3,6 +3,7 @@ package jp.ac.chiba_fjb.asaka.ref.sdkrefs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(Menu.NONE, 0, 0, "Menu1").setIcon(R.mipmap.ic_launcher_round);
-        menu.add(Menu.NONE, 1, 1, "Menu2").setIcon(R.mipmap.ic_launcher);
-        return super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
