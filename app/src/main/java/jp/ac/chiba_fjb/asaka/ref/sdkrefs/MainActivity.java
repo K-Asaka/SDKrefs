59f94705041f1304d917ff2a5ad2ccc052912464
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2017-04-01 09:15:49 +0900

    [add] Toast sample.

--- app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
@@ -2,12 +2,16 @@ package jp.ac.chiba_fjb.asaka.ref.sdkrefs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // トーストの表示
+        Toast.makeText(this, "トースト表示", Toast.LENGTH_LONG).show();
+
     }
 }
