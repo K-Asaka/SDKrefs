b387c0e64654cede8f45ec6bea8fe3d5a649a566
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2017-04-01 09:23:06 +0900

    [mod] OptionsMenu sample. Deleted ProgressDialog code.

--- app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
@@ -1,51 +1,21 @@
 package jp.ac.chiba_fjb.asaka.ref.sdkrefs;
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
-
-    ProgressDialog progressDialog;
-    int DIALOG_DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // ProgressDialogの生成
-        progressDialog = getProgressDialog(this);
-        progressDialog.show();
-
-        // 3秒後にダイアログを消す
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                progressDialog.dismiss();
-            }
-        }, DIALOG_DISPLAY_LENGTH);
-
     }
 
-    // ProgressDialogインスタンスを生成
-    private ProgressDialog getProgressDialog(Context ctx) {
-        ProgressDialog dialog = new ProgressDialog(ctx);
-
-        // プログレススタイルを設定
-        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-
-        // キャンセル可能に設定
-        dialog.setCancelable(true);
-
-        // タイトルを設定
-        dialog.setTitle("タイトル");
-
-        // メッセージを設定
-        dialog.setMessage("メッセージ");
-
-        return dialog;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, 0, 0, "Menu1");
+        menu.add(Menu.NONE, 1, 1, "Menu2");
+        return super.onCreateOptionsMenu(menu);
     }
 }
