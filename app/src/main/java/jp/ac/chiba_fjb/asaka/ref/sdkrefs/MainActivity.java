ea31735836d91b401935710319d62a345a6f35cd
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2017-04-02 09:37:19 +0900

    [mod] Added menu on Actionbar. Deleted menu events.

--- app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
@@ -4,8 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,21 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(Menu.NONE, 0, Menu.NONE, "Menu1");
-        menu.add(Menu.NONE, 1, Menu.NONE, "Menu2");
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case 0:
-                Toast.makeText(this, "Menu1", Toast.LENGTH_SHORT).show();
-                break;
-            case 1:
-                Toast.makeText(this, "Menu2", Toast.LENGTH_SHORT).show();
-                break;
-        }
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
         return true;
     }
+
 }
