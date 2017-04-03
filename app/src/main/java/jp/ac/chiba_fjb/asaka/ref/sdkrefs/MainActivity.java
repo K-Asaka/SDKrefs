6cbc6202dd91003546188a3177f381865bde1813
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2017-04-04 00:26:09 +0900

    [add] Added event listener on SearchView.

--- app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
@@ -2,10 +2,14 @@ package jp.ac.chiba_fjb.asaka.ref.sdkrefs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import static android.support.v7.widget.SearchView.*;
+
+public class MainActivity extends AppCompatActivity implements OnQueryTextListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,21 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.action_bar_menu, menu);
-        return true;
+
+        SearchView searchView = (SearchView)menu.findItem(R.id.action_search).getActionView();
+        searchView.setOnQueryTextListener(this);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
+        return false;
     }
 
 }
