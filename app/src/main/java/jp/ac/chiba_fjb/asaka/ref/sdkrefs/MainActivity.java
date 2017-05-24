2fa1bb037552c1095b270f596ace39a1f2079394
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2017-05-24 21:13:22 +0900

    [chg] Custom layout on ActionBar. Deleted ActionProvider code.

--- app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/asaka/ref/sdkrefs/MainActivity.java
@@ -2,12 +2,7 @@ package jp.ac.chiba_fjb.asaka.ref.sdkrefs;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.View;
-
 
 public class MainActivity extends Activity {
 
@@ -15,27 +10,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setActionBar();
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
-        return true;
-    }
-
-    private void setActionBar() {
-        getActionBar().setDisplayShowCustomEnabled(true);
-        getActionBar().setDisplayShowTitleEnabled(false);
-        getActionBar().setIcon(null);
-        getActionBar().setDisplayShowHomeEnabled(false);
-        getActionBar().setDisplayUseLogoEnabled(false);
-
-        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View actionBarLayout = inflater.inflate(R.layout.sample_action_provider, null);
-        ActionBar.LayoutParams params = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
-        getActionBar().setCustomView(actionBarLayout, params);
-
+        ActionBar actionBar = getActionBar();
+        actionBar.setCustomView(R.layout.actionbar);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
     }
 
 }
